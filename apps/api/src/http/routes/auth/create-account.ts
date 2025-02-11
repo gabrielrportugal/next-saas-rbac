1fae8d63e7dc6b0113fff1a04095665985b5e449
gabrielrportugal <gabrielrportugal@outlook.com>
2025-02-11 16:19:21 -0300

    feat(api): create account route and functionalities

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,40 @@
+import { z } from 'zod'
+import { hash } from 'bcryptjs'
+import { prisma } from '@/lib/prisma';
+
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post('/users', 
+    {
+      schema: {
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6)
+        })
+      }
+    },async (request, reply) => {
+      
+      const { email, name, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({where: {email}})
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({message: 'User with same e-mail already exists.'})
+      }
+
+      const passwordHash = await hash(password, 4)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email, 
+          passwordHash
+        }
+      })
+
+      return reply.status(201).send({message: 'User created successfully.'})
+  })
+}
